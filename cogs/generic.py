d43eb5c757d770946973d975b1dba7fa5b9465da
Heroic <anupshrma2007@gmail.com>
2021-08-16 08:39:24 +0530

    Add timestamp to usage embeds

--- cogs/generic.py
+++ cogs/generic.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import json
 import logging
 import os
+import datetime
 from dotenv import load_dotenv
 
 try:
@@ -41,6 +42,7 @@ async def record_usage(self, ctx):
     with open('./config.json') as file:
         data = json.load(file)
         logcheck = data['log_function_usage']
+        file.close()
 
     if not logcheck:
         log.debug(f'Logcheck is False, Returning')
@@ -58,6 +60,8 @@ async def record_usage(self, ctx):
     embed.add_field(name="Guild Name", value=ctx.guild.name, inline=False)
     embed.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
     embed.add_field(name="Message content", value=ctx.message.content, inline=False)
+    embed.set_footer(text=datetime.datetime.utcnow(), icon_url=ctx.author.avatar_url)
+
     log.debug(f"Created Embed")
 
     log.debug(f"Sending Embed")
@@ -65,7 +69,7 @@ async def record_usage(self, ctx):
     log.debug(f"Embed Sent, Error Handler Quit")
 
 
-async def finish_usage(self, ctx):
+async def finish_usage(self, ctx: commands.Context):
     log.info(f"{ctx.author} finished using {ctx.command} in {ctx.guild}")
 
     logcheck = ''
@@ -73,6 +77,7 @@ async def finish_usage(self, ctx):
     with open('./config.json') as file:
         data = json.load(file)
         logcheck = data['log_function_usage']
+        file.close()
 
     if not logcheck:
         log.debug(f'Logcheck is False, Returning')
@@ -85,13 +90,15 @@ async def finish_usage(self, ctx):
     log.debug(f"Creating Embed")
     embed = discord.Embed(title=":medal: Command Finished", color=0x00FF00)
 
-    embed.add_field(name="Author Username", value=ctx.author, inline=False)
-    embed.add_field(name="Author ID", value=ctx.author.id, inline=False)
-    embed.add_field(name="Guild Name", value=ctx.guild.name, inline=False)
-    embed.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
+    embed.add_field(name="Author Username", value=ctx.author,     inline=False)
+    embed.add_field(name="Author ID",       value=ctx.author.id,  inline=False)
+    embed.add_field(name="Guild Name",      value=ctx.guild.name, inline=False)
+    embed.add_field(name="Guild ID",        value=ctx.guild.id,   inline=False)
     embed.add_field(name="Message content", value=ctx.message.content, inline=False)
-    log.debug(f"Created Embed")
 
+    embed.set_footer(text=datetime.datetime.utcnow(), icon_url=ctx.author.avatar_url)
+
+    log.debug(f"Created Embed")
     log.debug(f"Sending Embed")
     await channel.send(embed=embed)
     log.debug(f"Embed Sent, Error Handler Quit")
