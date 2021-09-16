b71036a84be97ba068b2e1ddda5b2f28543c623a
Deepesh Nimma <deepnimma@gmail.com>
2021-09-16 08:54:11 +0530

    change cotd function to do nothing, will work on in development branch

--- cogs/cotd.py
+++ cogs/cotd.py
@@ -24,34 +24,12 @@ class COTD(commands.Cog, description="Commands related to COTD Standings"):
         name="COTD",
         aliases=["cotd", "totd", "stats", "cotdstats"],
         help="COTD Stats for a given player, if none is given it will use your stored username",
+        hidden=True,
     )
     @commands.before_invoke(record_usage)
     @commands.after_invoke(finish_usage)
     async def cotd(self, ctx: commands.Command, username: str = None) -> None:
-        log.debug(f"Checking Username")
-
-        user_id = ""
-
-        if username is None:
-            log.debug(f"No Username Given, Checking if in file")
-            if not username_functions.check_trackmania_username_in_file(ctx):
-                log.error(f"Username not in File")
-                return None
-            else:
-                log.debug(f"Username in File, Getting Id")
-                user_id = username_functions.get_trackmania_id(ctx)
-
-        else:
-            log.debug(f"Username Given, Checking if Valid")
-
-            if not username_functions.check_username(username):
-                log.error(f"Not a Valid Username")
-                return None
-            else:
-                log.debug(f"Valid Username, Getting ID from API")
-                user_id = username_functions.get_trackmania_id_from_api(ctx, username)
-
-        await ctx.send(user_id)
+        return None
 
 
 def setup(client):
