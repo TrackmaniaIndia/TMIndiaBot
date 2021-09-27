4f127365e5011a98aa047778b26b8ec48c402e44
nottcurious <deepnimma@gmail.com>
2021-09-27 21:16:43 +0530

    quote function

---
+++ cogs/quoting.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands, tasks
+import json
+import logging
+from datetime import datetime
+from dotenv import load_dotenv
+from itertools import cycle
+
+import functions.logging.convert_logging as convert_logging
+import functions.common_functions.common_functions as common_functions
+import functions.cog_helpers.generic_functions
+from functions.logging.usage import record_usage, finish_usage
+from functions.task_helpers.pingapi import ping_api
+from functions.other_functions.get_data import get_version
+
+load_dotenv()
+# log_level = os.getenv("LOG_LEVEL")
+# version = os.getenv("VERSION")
+# discord_log_level = os.getenv("DISCORD_LOG_LEVEL")
+
+
+log = logging.getLogger(__name__)
+log = convert_logging.get_logging()
+
+version = get_version()
+
+class Quote(commands.Cog, description='Quoting Functions'):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['q'], help='Quotes a Message -> Format "Message" - Author')
+    @commands.has_any_role('admin', 'Bot Developer')
+    @commands.before_invoke(record_usage)
+    @commands.after_invoke(finish_usage)
+    async def quote(self, ctx: commands.Context, *, message):
+        message, author = message.split(' - ')
+
+        print(f'{message}, {author}')
+
+    @commands.command(help='Quotes a Random Quote')
+    @commands.before_invoke(record_usage)
+    @commands.after_invoke(finish_usage)
+    async def randquote(self, ctx: commands.Context):
+        pass
+
+def setup(client):
+    client.add_cog(Quote(client))
---
+++ functions/cog_helpers/quote_functions.py
@@ -0,0 +1,48 @@
+import os
+import json
+import logging
+import functions.logging.convert_logging as convert_logging
+from datetime import datetime
+
+log = logging.getLogger(__name__)
+log = convert_logging.get_logging()
+
+def save(message, author):
+    log.debug(f'Saving {message} by {author} at {datetime.now()}')
+    log.debug(f'Opening JSON File')
+
+    quotes = []
+    date_created_unformatted = datetime.now()
+    # date_created = date_created_unformatted.strftime('%d %B, %Y - %I:%M:%S %p')
+    date_created = date_created_unformatted.strftime('%c')
+
+    with open('./json_data/quotes.json', 'r') as file:
+        log.debug(f'Loading JSON file')
+        quotes = json.load(file)
+        log.debug(f'Opened JSON File')
+
+    new_quote_dict = {
+        "Message": message,
+        "Author": author,
+        "Date Created": date_created,
+        "Number": int(get_number_of_quotes() + 1)
+    }
+
+    quotes += new_quote_dict
+
+    log.debug(f'Dumping to File')
+    with open('./json_data/quotes.json', 'w') as file:
+        json.dump(quotes, file, indent=4)
+    log.debug(f'Dumped to File')
+    log.debug(f'Returning')
+    return None
+
+def get_number_of_quotes():
+    log.debug(f'Opening JSON File')
+    with open('./json_data/quotes.json', 'r') as file:
+        log.debug(f'Loading JSON file')
+        quotes = json.load(file)
+        log.debug(f'Read JSON file, returning length of quotes array')
+
+        return len(quotes['quotes'])
+
