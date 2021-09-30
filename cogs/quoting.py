29c5c170d2f7486ed1af908b423b8f34a4a8ff1a
Deepesh Nimma <deepnimma@gmail.com>
2021-09-30 09:33:40 +0530

    change variables to follow python naming conventions

--- cogs/quoting.py
+++ cogs/quoting.py
@@ -62,32 +62,32 @@ class Quote(commands.Cog, description="Quoting Functions"):
     @commands.after_invoke(finish_usage)
     async def viewquotes(self, ctx: commands.Context, mention: discord.User = None):
         if mention != None:
-            userId = mention.id
+            user_id = mention.id
             user = mention
         else: 
-            userId = ctx.author.id
+            user_id = ctx.author.id
             user = ctx.message.author
 
-        userQuotes = quote_functions.getQuotesById(userId)
+        user_quotes = quote_functions.getQuotesById(user_id)
 
-        if not userQuotes:
-            embed = discord.Embed(title="No quotes for " + user.name, description="Add one by using `$quote`", color=discord.Colour.random())
+        if not user_quotes:
+            embed = discord.Embed(title="No quotes for " + user.name, description="Add one by using `$quote`", color=discord.Colour.red())
             await ctx.send(embed=embed)
             return None
 
-        embed=discord.Embed(title=f"Quotes by {user.name}", description=f"{user.name} Has uploaded {len(userQuotes)} Quotes", colour=discord.Colour.random())
+        embed=discord.Embed(title=f"Quotes by {user.name}", description=f"{user.name} Has uploaded {len(user_quotes)} Quotes", colour=discord.Colour.random())
 
-        if len(userQuotes) > 1:
-            loopAmount = 2 
+        if len(user_quotes) > 1:
+            loop_amount = 2 
         else:
-            loopAmount = len(userQuotes)
+            loop_amount = len(user_quotes)
 
-        for i in range(loopAmount):
-            quote = userQuotes[i]
+        for i in range(loop_amount):
+            quote = user_quotes[i]
             embed.add_field(name=f'{quote["Message"]}', value=f' - {quote["Author"]}', inline=True)
         
-        if len(userQuotes) > 1:
-            embed.set_footer(text=f"{len(userQuotes) - loopAmount} More...")
+        if len(user_quotes) > 1:
+            embed.set_footer(text=f"{len(user_quotes) - loop_amount} More...")
 
         await ctx.send(embed=embed)
 
