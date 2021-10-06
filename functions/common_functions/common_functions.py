4885d89f343e3393279e0f5d569732b2ae91b291
Ashwin Sharma <anupshrma2007@gmail.com>
2021-10-06 16:34:01 +0530

    add checkKey()

--- functions/common_functions/common_functions.py
+++ functions/common_functions/common_functions.py
@@ -10,6 +10,18 @@ log = convert_logging.get_logging()
 def get_random_color() -> discord.Colour:
     return discord.Colour.random()
 
+def checkKey(d: dict, key: str) -> bool:
+    found = False
+    try:
+        if d[key]:
+            found = True
+        else:
+            found = False
+    except KeyError or IndexError as e:
+        found = False
+    
+    return found
+
 
 if __name__ == "__main__":
     for i in range(0, 100):
