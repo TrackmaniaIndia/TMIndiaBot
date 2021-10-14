cfb6d6d322c0bc98aa815922dc91ccd315962893
awsomemanNever <thebluechalk@gmail.com>
2021-10-14 18:21:21 +0530

    cleaned up file check

--- functions/checks/file_check.py
+++ functions/checks/file_check.py
@@ -10,83 +10,50 @@ def check():
     """Checks if the Files Exists"""
     log.debug(f"Checking files")
 
-    json_files = (
-        "./data/json_data/announcement_channels.json",
-        "./data/json_data/config.json",
-        "./data/json_data/prefixes.json",
-        "./data/json_data/statuses.json",
-        "./data/json_data/tm2020_usernames.json",
-    )
-    cog_files = (
-        "./cogs/channel_commands.py",
-        "./cogs/cotd.py",
-        "./cogs/generic.py",
-        "./cogs/moderation.py",
-        "./cogs/owner.py",
-        "./cogs/tm_commands.py",
-        "./cogs/username_commands.py",
-    )
-    data_files = ("./data/times_run.txt",)
-    functions = (
-        ("./functions/ciphers/vigenere_cipher.py",),  # Ciphers
-        (
-            "./functions/cog_helpers/cotd_functions.py",
-            "./functions/cog_helpers/generic_functions.py",
-            "./functions/cog_helpers/tm_commands_functions.py",
-        ),  # Cog Helpers
-        ("./functions/common_functions/common_functions.py",),  # Common Functions
-        ("./functions/custom_errors/custom_errors.py",),  # Custom Errors
-        (
-            "./functions/logging/convert_logging.py",
-            "./functions/logging/usage.py",
-        ),  # Logging
-        ("./functions/other_functions/get_data.py",),  # Other Functions
-        ("./functions/task_helpers/pingapi.py",),  # Task Helpers
-        (
-            "./functions/tm_username_functions/username_functions.py",
-        ),  # TM Username Functions
-    )
+    # dictionary of all files
+    files = {
+        "json": (
+                 "./data/json_data/announcement_channels.json",
+                 "./data/json_data/config.json",
+                 "./data/json_data/prefixes.json",
+                 "./data/json_data/statuses.json",
+                 "./data/json_data/tm2020_usernames.json",
+                 ),
+        "cog": (
+                "./cogs/channel_commands.py",
+                "./cogs/cotd.py",
+                "./cogs/generic.py",
+                "./cogs/moderation.py",
+                "./cogs/owner.py",
+                "./cogs/tm_commands.py",
+                "./cogs/username_commands.py",
+                ),
+        "data": (
+                 "./data/times_run.txt",
+                 ),
+        "functions": (
+                      "./functions/ciphers/vigenere_cipher.py", # ciphers
+                      "./functions/cog_helpers/cotd_functions.py", #            \
+                      "./functions/cog_helpers/generic_functions.py", #         | Cog Helpers
+                      "./functions/cog_helpers/tm_commands_functions.py", #     /
+                      "./functions/common_functions/common_functions.py", # common functions
+                      "./functions/custom_errors/custom_errors.py", # custom errors
+                      "./functions/logging/convert_logging.py", #  \
+                      "./functions/logging/usage.py", #            / Logging
+                      "./functions/other_functions/get_data.py", # other functions
+                      "./functions/task_helpers/pingapi.py", # task helpers
+                      "./functions/tm_username_functions/username_functions.py", # TM username functions
+                      ),
+    }
 
-    log.debug(f"Beginning Check")
-    log.debug(f"Checking all JSON Files")
+    print(f"Beginning Check")
+    print(f"Checking all JSON Files")
 
-    for filename in json_files:
-        if not os.path.exists(filename):
-            log.error(f"{filename} does not exist, exiting")
-            exit()
-        else:
-            log.debug(f"{filename} exists")
+    # checking for the existence of files
+    for types in files:
+        for file in files[types]:
+            if not os.path.exists(file):
+                log.error(f"{file} does not exist")
+        log.debug(f"{types} checked")
 
-    log.debug(f"Checked all JSON Files")
-    log.debug(f"Checking all Cog Files")
-
-    for filename in cog_files:
-        if not os.path.exists(filename):
-            log.error(f"{filename} does not exist, exiting")
-            exit()
-        else:
-            log.debug(f"{filename} exists")
-
-    log.debug(f"Checked all Cog Files")
-    log.debug(f"Checking all Data Files")
-
-    for filename in data_files:
-        if not os.path.exists(filename):
-            log.error(f"{filename} does not exist, exiting")
-            exit()
-        else:
-            log.debug(f"{filename} exists")
-
-    log.debug(f"Checked all Data Files")
-    log.debug(f"Checking all Functions")
-
-    for function_group in functions:
-        for filename in function_group:
-            if not os.path.exists(filename):
-                log.error(f"{filename} does not exist, exiting")
-                exit()
-            else:
-                log.debug(f"{filename} exists")
-
-    log.debug(f"Checked all Functions")
-    log.debug(f"All files exist, goodluck")
+    log.debug(f"Checked all files")
