44c42d1aa8c4ea8cee301693278cce18cb106df0
Deepesh Nimma <deepnimma@gmail.com>
2021-10-21 16:38:08 +0530

    prefix command

--- cogs/generic.py
+++ cogs/generic.py
@@ -15,87 +15,42 @@ from util.guild_ids import guild_ids
 log = convert_logging.get_logging()
 version = get_version()
 
+DEFAULT_PREFIX = "--"
+
 
 class Generic(commands.Cog, description="Generic Functions"):
-    first_time = True
-    statuses = []
-
     def __init__(self, client):
         self.client = client
-        self.first_time = True
 
-        log.debug(f"Getting Statuses")
-        self.statuses = cycle([])
-        with open("./data/json/statuses.json", "r") as file:
-            log.debug(f"Status Loading File")
-            self.statuses = json.load(file)["statuses"]
-            self.statuses.append(f"Version: {version}! Online and Ready")
-            self.statuses = cycle(self.statuses)
-            log.debug(f"Status File Loaded")
-            file.close()
-        log.debug(f"Received Statuses")
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await self.client.change_presence(
-            status=discord.Status.online,
-            activity=discord.Game(f"Version: {version}! Online and Ready"),
-        )
-        log.critical("Bot Logged In")
-
-        times_run = 0
-        log.debug(f"Reading Value from Times Run File")
-        with open("./data/times_run.txt", "r") as file:
-            times_run = int(file.readline())
-
-        times_run += 1
-
-        log.debug(f"Starting Keep Alive Loop")
-        keep_alive.start(self.client)
-        log.debug(f"Started Keep Alive Loop")
-        log.debug(f"Start Change Status Loop")
-        change_status.start(self.client, self.first_time, self.statuses)
-        log.debug(f"Started Change Status Loop")
-
-        log.debug(f"Getting Announcement Channels")
-        with open("./data/json/announcement_channels.json", "r") as file:
-            channels = json.load(file)
-
-        print_system_info()
-
-        for announcement_channel in channels["announcement_channels"]:
-            log.debug(f"Sending Message in {announcement_channel}")
-
-            channel = self.client.get_channel(int(announcement_channel))
-            try:
-                await channel.send(
-                    f"Bot is Ready, Version: {version} - Times Run: {times_run} - Time of Start: {datetime.now()}"
-                )
-                log.debug(f"Sent Message to {announcement_channel}")
-                if (
-                    int(announcement_channel) == 880771916099641364
-                    or int(announcement_channel) == 880628511512096778
-                ):
-                    continue
-            except:
-                log.debug(f"Can't Send Message to {announcement_channel}")
-                continue
-        log.debug(f"Writing TimesRun to File")
-        with open("./data/times_run.txt", "w") as file:
-            print(times_run, file=file)
-
-        log.info(f"Bot now Usable")
-
-    @commands.slash_command(
-        guild_ids=guild_ids,
+    @commands.command(
+        # guild_ids=guild_ids,
+        name="ping",
         aliases=["latency", "pong", "connection"],
         help="Shows the Ping/Latency of the Bot in milliseconds.",
         brief="Shows Bot Ping.",
     )
-    @commands.before_invoke(record_usage) # Doesn't Work With Slash Commands, Must Check or Change
+    # @commands.slash_command(guild_ids=guild_ids, name='ping', description='Get ping of bot to discord api in milliseconds')
+    @commands.before_invoke(
+        record_usage
+    )  # Doesn't Work With Slash Commands, Must Check or Change
     @commands.after_invoke(finish_usage)
-    async def ping(self, ctx: commands.Context):
-        await ctx.respond("Pong! {}ms".format(round(self.client.latency * 1000, 2)))
+    async def _ping(self, ctx: commands.Context):
+        await ctx.reply(
+            "Pong! {}ms".format(round(self.client.latency * 1000, 2)),
+            mention_author=False,
+        )
+
+    @commands.command(
+        name="prefix", help="Changes Prefix to Given prefix", brief="Changes Prefix"
+    )
+    @commands.before_invoke(record_usage)
+    @commands.after_invoke(finish_usage)
+    @commands.has_any_role("admin", "Moderator", "Bot Developer")
+    @commands.guild_only()
+    async def _prefix(self, ctx: commands.Context, prefix: str):
+        change_prefix(ctx, prefix)
+        await ctx.reply("Prefix Changed to {}".format(prefix), mention_author=False)
+        log.info(f"prefix successfully changed to {prefix} for {ctx.guild.name}")
 
 
 def setup(client):
--- util/cog_helpers/generic_helper.py
+++ util/cog_helpers/generic_helper.py
@@ -2,8 +2,10 @@ import util.logging.convert_logging as cl
 import psutil
 import platform
 import json
+from discord.ext import commands
 
 log = cl.get_logging()
+DEFAULT_PREFIX = "--"
 
 
 def print_system_info():
@@ -37,9 +39,40 @@ def print_system_info():
 
 
 def get_version():
+    """Get's Current Version of the Bot"""
     with open("./data/config.json") as file:
         config = json.load(file)
         version = config["bot_version"]
         file.close()
 
     return version
+
+
+def change_prefix(ctx: commands.Context, prefix: str) -> bool:
+    """Changes the Prefix for a Given Server
+
+    Args:
+        ctx (commands.Context): Application Context for ID and Name
+        prefix (str): The Prefix to Change to
+
+    Returns:
+        bool: > True if Success, > False if failure
+    """
+    log.info(f"Changing Prefix in {ctx.guild}")
+
+    with open("./data/json/prefixes.json", "r") as file:
+        log.debug("Opening Prefixes JSON")
+        prefixes = json.load(file)
+        file.close()
+
+    log.debug(f"Changing Prefix")
+    if prefix != "":
+        prefixes[str(ctx.guild.id)] = [prefix, DEFAULT_PREFIX]
+    else:
+        prefixes[str(ctx.guild.id)] = DEFAULT_PREFIX
+    log.debug(f"Changed Prefix")
+
+    with open("./data/json/prefixes.json", "w") as file:
+        log.debug("Dumping Prefixes to File")
+        json.dump(prefixes, file, indent=4)
+        file.close()
