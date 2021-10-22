7d8246db2d5fd54d1aad0bff5ef3c5c9ac9ce8f2
Deepesh Nimma <deepnimma@gmail.com>
2021-10-22 10:17:17 +0530

    rework record usage and finish usage and add new logging commands for slash command future use

--- util/logging/usage.py
+++ util/logging/usage.py
@@ -29,37 +29,6 @@ async def record_usage(self, ctx: commands.Context) -> None:
         f"{ctx.author} used {ctx.command} at {ctx.message.created_at} in {ctx.guild}"
     )
 
-    log_check = ""
-
-    with open("./data/config.json") as file:
-        data = json.load(file)
-        log_check = data["log_function_usage"]
-        file.close()
-
-    if not log_check:
-        log.debug(f"log_check is False, Returning")
-        return
-    log.debug(f"log_check is True, Sending Message")
-
-    log.debug(f"Sending Message to Error Channel")
-    channel = self.client.get_channel(876442289382248468)
-
-    log.debug(f"Creating Embed")
-    embed = discord.Embed(title=":clapper: Command Used", color=0x23FFFF)
-
-    embed.add_field(name="Author Username", value=ctx.author, inline=False)
-    embed.add_field(name="Author ID", value=ctx.author.id, inline=False)
-    embed.add_field(name="Guild Name", value=ctx.guild.name, inline=False)
-    embed.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
-    embed.add_field(name="Message content", value=ctx.message.content, inline=False)
-    embed.set_footer(text=datetime.datetime.utcnow(), icon_url=ctx.author.avatar_url)
-
-    log.debug(f"Created Embed")
-
-    log.debug(f"Sending Embed")
-    await channel.send(embed=embed)
-    log.debug(f"Embed Sent, Error Handler Quit")
-
 
 async def finish_usage(self, ctx: commands.Context) -> None:
     """Records Finish Usage of Commands
@@ -69,33 +38,19 @@ async def finish_usage(self, ctx: commands.Context) -> None:
     """
     log.info(f"{ctx.author} finished using {ctx.command} in {ctx.guild}")
 
-    log_check = ""
 
-    with open("./data/config.json") as file:
-        data = json.load(file)
-        log_check = data["log_function_usage"]
-        file.close()
+async def record_usage_slash(self, ctx: commands.Context) -> None:
+    """Records Usage of Slash Commands
 
-    if not log_check:
-        log.debug(f"log_check is False, Returning")
-        return
-    log.debug(f"log_check is True, Sending Message")
+    Args:
+        ctx (commands.Context): Message Context
+    """
+    log.info(f"{ctx.author.name} used slash command -> {ctx.command} at {ctx.message.created_at} in {ctx.guild}")
+    
+async def finish_usage_slash(self, ctx: commands.Context) -> None:
+    """Records Finish Usage of Commands
 
-    log.debug(f"Sending Message to Error Channel")
-    channel = self.client.get_channel(876442289382248468)
-
-    log.debug(f"Creating Embed")
-    embed = discord.Embed(title=":medal: Command Finished", color=0x00FF00)
-
-    embed.add_field(name="Author Username", value=ctx.author, inline=False)
-    embed.add_field(name="Author ID", value=ctx.author.id, inline=False)
-    embed.add_field(name="Guild Name", value=ctx.guild.name, inline=False)
-    embed.add_field(name="Guild ID", value=ctx.guild.id, inline=False)
-    embed.add_field(name="Message content", value=ctx.message.content, inline=False)
-
-    embed.set_footer(text=datetime.datetime.utcnow(), icon_url=ctx.author.avatar_url)
-
-    log.debug(f"Created Embed")
-    log.debug(f"Sending Embed")
-    await channel.send(embed=embed)
-    log.debug(f"Embed Sent, Error Handler Quit")
+    Args:
+        ctx (commands.Context): Message Context
+    """
+    log.info(f"{ctx.author} finished using slash command: {ctx.command} in {ctx.guild}")
