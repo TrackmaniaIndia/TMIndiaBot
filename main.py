d5cb9c8a8c64b371ee59d1dd9ca15625965ec3fa
Deepesh Nimma <deepnimma@gmail.com>
2021-10-27 18:39:34 +0530

    create threads outside of __name__ function

--- main.py
+++ main.py
@@ -28,17 +28,20 @@ log.info(f'Creating Client')
 client = commands.Bot(command_prefix=get_prefix, intents=discord.Intents.default())
 log.info(f'Created Client')
 
+log.info('Creating Thread for NPM Module')
+npm_module = threading.Thread(target=subprocess.run, args=(['npm', 'run', 'start'],), kwargs={'shell': True})
+log.info('Thread Created for NPM Module')
+
+log.info('Starting Bot Startup')
 if __name__ == '__main__':
     file_check()
     
-    # log.info('Creating Thread for NPM Module')
-    # npm_module = threading.Thread(target=subprocess.run, args=(['npm', 'run', 'start'],), kwargs={'shell': True})
-    # log.info('Thread Created for NPM Module, Starting Process')
-    # npm_module.start()
-    # log.info('NPM Module Thread Started, Waiting 2s before we continue')
-    # time.sleep(2)
+    log.info('Starting NPM Module thread')
+    npm_module.start()
+    log.info('NPM Module Thread Started, Waiting 3s before we continue')
+    time.sleep(3)
     
-    # log.info('NPM Module Initialized, Resuming Bot INIT')
+    log.info('NPM Module Initialized, Resuming Bot INIT')
 
     # Loading Cogs
     log.info('Loading Slash Cogs')
