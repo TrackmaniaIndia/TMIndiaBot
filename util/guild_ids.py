bb8ab5afb5c77da217c3974c6fc57a6185311cdf
Deepesh Nimma <deepnimma@gmail.com>
2021-10-27 14:52:54 +0000

    username update

--- util/guild_ids.py
+++ util/guild_ids.py
@@ -1,2 +1,2 @@
-# guild_ids = [876042400005505066, 805313762663333919] # For Main Bot
-guild_ids = [876042400005505066]  # For Testing Bot
+guild_ids = [876042400005505066, 805313762663333919] # For Main Bot
+# guild_ids = [876042400005505066]  # For Testing Bot
