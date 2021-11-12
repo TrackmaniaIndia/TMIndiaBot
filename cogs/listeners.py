a3ade7ef81f2a58fe62ce431a1b58a0903cf4786
Deepesh Nimma <deepnimma@gmail.com>
2021-11-12 16:17:49 +0530

    comment the waiting

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -57,12 +57,12 @@ class Listeners(commands.Cog, description="Generic Functions"):
         change_status.start(self.client, self.first_time, self.statuses)
         log.debug(f"Started Change Status Loop")
 
-        log.info(f"Starting Loop to Wait for a correct time")
-        curr_time = str(datetime.now().strftime("%M"))
-        while curr_time != "28" and curr_time != "58":
-            time.sleep(0.5)
-            curr_time = str(datetime.now().strftime("%M"))
-        log.info(f"Current Time is {datetime.now()}, continuing bot init")
+        # log.info(f"Starting Loop to Wait for a correct time")
+        # curr_time = str(datetime.now().strftime("%M"))
+        # while curr_time != "28" and curr_time != "58":
+        #     time.sleep(0.5)
+        #     curr_time = str(datetime.now().strftime("%M"))
+        # log.info(f"Current Time is {datetime.now()}, continuing bot init")
 
         log.debug(f"Getting Announcement Channels")
         with open("./data/json/announcement_channels.json", "r") as file:
