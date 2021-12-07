f5a0eba8c9267faed063b63dd4cb38464bd95b51
Deepesh Nimma <deepnimma@gmail.com>
2021-12-07 16:18:09 +0530

    comments

--- main.py
+++ main.py
@@ -1,47 +1,62 @@
 import discord
-from discord.ext import commands
 import os
-from dotenv import load_dotenv
-import json
 import subprocess
 import threading
-import time
-
-from datetime import datetime
-
-# import jishaku
 
 import util.logging.convert_logging as cl
-import util.discord.easy_embed as ezembed
-from util.before_launch.file_check import file_check
-from util.constants import GUILD_IDS
 
+from dotenv import load_dotenv
+
+from util.before_launch.file_check import file_check
+
+# Setting up logging
 log = cl.get_logging()
 log.info(f"Logging Setup for main.py")
 
+# Getting Bot Token
 log.info(f"Reading Bot Token")
 load_dotenv()
 BOT_TOKEN = os.getenv("BOTTOKEN")
 log.info(f"Got Bot Token")
 
+# Creating Bot with the following intents:
+#   bans
+#   dm_messages
+#   dm_reactions
+#   dm_typing
+#   emojis
+#   emojis_and_stickers
+#   guild_messages
+#   guild_reactions
+#   guild_typing
+#   integrations
+#   invites
+#   members
+#   messages
+#   reactions
+#   typing
+#   value
+#   voice_states
+#   webhooks
 log.info(f"Creating Client")
 client = discord.Bot(intents=discord.Intents.default())
 log.info(f"Created Client")
 
+# This Thread runs the TMIndiaBotApi, clone from https://github.com/artifexdevstuff/TMIndiaBotApi
 log.info("Creating Thread for NPM Module")
-# npm_module = threading.Thread(
-#     target=subprocess.run, args=(["npm", "run", "start"],), kwargs={"shell": True}
-# )
 npm_module = threading.Thread(
     target=subprocess.run, args=("npm run start",), kwargs={"shell": True}
 )
 
 log.info("Thread Created for NPM Module")
 
+# Starting Bot
 log.info("Starting Bot Startup")
 if __name__ == "__main__":
+    # Checking for Important Files
     file_check()
 
+    # Starting NPM Module
     log.info("Starting NPM Module thread")
     npm_module.start()
     log.info("NPM Module Thread Started")
@@ -55,5 +70,5 @@ if __name__ == "__main__":
             client.load_extension(f"cogs.{filename[:-3]}")
     log.info("Loaded Slash Cogs")
 
-    # Running Client
+    # Running Bot Client
     client.run(BOT_TOKEN)
