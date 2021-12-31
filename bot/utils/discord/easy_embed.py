bc59d300ebbd6dfeb1e53e1ba3b69e8e9a22dfd9
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2021-12-31 09:24:52 +0530

    viewadder and easy embed

---
+++ bot/utils/discord/easy_embed.py
@@ -0,0 +1,47 @@
+import typing
+
+from datetime import datetime, timedelta, timezone
+
+import discord
+
+from bot.log import get_logger
+
+log = get_logger(__name__)
+
+
+def create_embed(
+    title: str,
+    description: str = "",
+    color: typing.Union[int, discord.Colour] = None,
+    url: str = None,
+) -> discord.Embed:
+    """Creates an Embed with Basic Data Fields Filled Out
+    Args:
+        title (str): Title of the Embed
+        description (str, optional): Description of the Embed. Defaults to None.
+        color (str, optional): Color of the Embed. Defaults to None.
+        url (str, url): Url to be added to the embed
+    Returns:
+        discord.Embed: Final Embed Returned with Data Fields Inside
+    """
+
+    if color is None:
+        log.debug("Colour is None, Assigning Random Colour")
+        color = common_functions.get_random_color()
+
+    # Creates an Embed with the Given Title, Description and Color
+    log.debug(
+        f"Creating Embed with Title - {title}, description - {description} and colour - {color}"
+    )
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=color,
+        url=discord.Embed.Empty if url is None else url,
+    )
+
+    # Adds the timestamp the embed was created on
+    embed.timestamp = datetime.now(timezone(timedelta(hours=5, minutes=30)))
+
+    log.debug(f"Returning {embed}")
+    return embed
