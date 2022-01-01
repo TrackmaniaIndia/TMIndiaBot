2cf3b413cb0931d52a8927b6a66ee2893e14a037
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2022-01-01 17:03:39 +0530

    quote utils

---
+++ bot/cogs/quoting/__init__.py
@@ -0,0 +1,6 @@
+import discord
+from discord.commands import permissions
+from discord.commands.commands import Option
+from discord.ext import commands
+
+import bot.utils.discord.easy_embed as ezembed
