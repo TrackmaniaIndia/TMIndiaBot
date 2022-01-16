c60eb01f66d5476c31b17e71a0bff56039ea889e
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2022-01-16 11:19:07 +0530

    Add  update

---
+++ bot/cogs/trackmania/campaign_update.py
@@ -0,0 +1,101 @@
+from discord.commands import Option
+from discord.ext import commands
+from discord.ext.pages import Paginator
+
+from bot.utils.discord.easy_embed import EZEmbed
+from bot import constants
+from bot.bot import Bot
+from bot.log import get_logger, log_command
+from bot.utils.trackmania import Leaderboards
+from bot.utils.discord.confirmation import Confirmer
+
+log = get_logger(__name__)
+
+
+class CampaignUpdate(commands.Cog):
+    def __init__(bot: Bot):
+        self.bot = bot
+
+    @commands.slash_command(
+        guild_ids=constants.Bot.default_guilds,
+        name="update_campaign_leaderboards",
+        description="Updates the leaderboards for campaigns",
+    )
+    async def _update_campaign_leaderboards_slash(
+        self,
+        ctx: commands.Context,
+        year: Option(str, "Choose the year", choices=["2020", "2021"]),
+        season: Option(
+            str, "Choose the season", choices=["Winter", "Spring", "Summer", "Fall"]
+        ),
+        firstfive: Option(
+            str, "Want to Update the First Five as Well?", choices=["True", "False"]
+        ),
+    ):
+        log_command(ctx, "update_campaign_leaderboards_slash")
+
+        firstfive = bool(firstfive)
+        log.info("Creating Confirmation Prompt")
+        confirm_continue = Confirmer()
+
+        # Changing the Button Labels
+        log.debug("Changing Button Labels")
+        confirm_continue.change_confirm_button(label="Yes, Continue")
+        confirm_continue.change_cancel_button(label="No, Stop!")
+        log.debug("changed Button Labels")
+
+        # Sending the Confrirmation Prompt
+        log.info("Sending Prompt")
+        message = await ctx.respond(
+            embed=EZEmbed.create_embed(
+                title="Are you sure you want to continue?",
+                description=f"This can take over 10 minutes, the bot will be unusable in this period\nYear: {year}\nSeason: {season}",
+                color=0xFF0000,
+            ),
+            view=confirm_continue,
+        )
+        log.info("Sent Confirmation Prompt")
+
+        # Awaiting for the Response to the Confirmation Prompt
+        log.info("Awaiting a response")
+        await confirm_continue.wait()
+        log.info("Response Received")
+
+        # Deleting the Confirmation Prompt
+        log.info("Deleting Original Message")
+        await message.delete_original_message()
+        log.info("Deleted Message")
+
+        # Checking if the Confirmation Prompt was Cancelled
+        if confirm_continue.value is False:
+            log.info(f"{ctx.author.name} does not want to continue")
+            return
+
+        log.info(f"{ctx.author.name} wants his username added")
+
+        # Getting the Fall Campaign IDs
+        log.info("Getting fall Campaign IDs")
+        fall_ids = Leaderboards.get_campaign_ids(year=year, season=season)
+        log.info("Got the Fall IDs")
+
+        # Starting Long Update Process using a seperate Thread to allow bot to complete other processes
+        log.info("Updating Leaderboards\nCreating Thread to Update Leaderboards")
+        leaderboard_update = threading.Thread(
+            target=Leaderboards.update_campaign_leaderboards,
+            args=(fall_ids, year, season, firstfive),
+        )
+        log.info("Thread Created to Update Leaderboards\nStarting Thread")
+        leaderboard_update.run()
+        log.info("Thread Finished\nLeaderboards Updates, Sleeping for 30s to save API")
+
+        # Sleeping 30s to allow our API requests to restore
+        time.sleep(30)
+
+        # Sending a "Finished" message
+        await ctx.send(
+            embed=EZEmbed.create_embed(
+                title=":white_check_mark: Success!",
+                description=f"Leaderboard Updates for Year: {year} and Season: {season}",
+                color=discord.Colour.green(),
+            )
+        )
