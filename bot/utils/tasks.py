d8bdb72dcb7dc4343827c6bfa626c9fbb885912b
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2022-01-21 14:50:59 +0530

    cotd reminders

--- bot/utils/tasks.py
+++ bot/utils/tasks.py
@@ -1,15 +1,18 @@
 import datetime
 import os
 import sys
-
-from discord.ext import tasks
+import json
 
 import discord
+from discord.ext import tasks
+
 from bot import constants
 from bot.api import APIClient
 from bot.bot import Bot
 from bot.log import get_logger
+from bot.utils.discord import ViewAdder
 from bot.utils.birthdays import Birthday
+from bot.utils.trackmania import TrackmaniaUtils
 
 log = get_logger(__name__)
 
@@ -28,6 +31,7 @@ async def change_status(bot: Bot, statuses: dict):
 
 @tasks.loop(minutes=15)
 async def keep_alive(bot: Bot):
+    """Keeps bot alive by sending a message to a designated channel and pings the API."""
     log.debug("Keeping Bot Alive")
 
     try:
@@ -51,7 +55,7 @@ async def totd_image_deleter(bot: Bot):
         log.debug("TOTD Image does not exist")
 
 
-@tasks.loop(time=datetime.time(hour=0, minute=30, second=0))
+@tasks.loop(time=datetime.time(hour=0, minute=30, second=0, tzinfo=datetime.timezone.utc))
 async def todays_birthday(bot: Bot):
     log.debug("Checking if it is anyone's birthday today")
     birthday_embed = Birthday.today_birthday()
@@ -73,7 +77,56 @@ async def todays_birthday(bot: Bot):
     log.debug("There is no birthday today")
     return
 
-
+# 15 minutes before the COTD
+@tasks.loop(time=datetime.time(hour=17, minute=45, second=0, tzinfo=datetime.timezone.utc))
+async def cotd_one_reminder(bot: Bot):
+    log.info('It is time for COTD #1')
+    
+    log.debug('Checking config file')
+    if constants.Bot.totd_reminders is False:
+        log.critical('TOTD Reminders are disabled')
+        return
+    
+    log.info('Pinging COTD #1 Role in TM India')
+    guild = bot.get_guild(constants.Guild.tmi_server)
+    
+    channel_id = constants.Channels.general if guild is not None else constants.Channel.testing_general
+    role_id = constants.Role.cotd_reminder_one if guild is not None else constants.Role.cotd_reminder_one_testing
+    
+    guild = guild if guild is not None else bot.get_guild(constants.Guild.testing_server)
+    
+    channel, role = bot.get_channel(channel_id), guild.get_role(role_id)
+    
+    message = f'{role.mention}: **COTD starts in 15 minutes**\n\n:medal: Past COTD results: <https://trackmania.io/#/totd>'
+    
+    log.debug('Sending Message')
+    await channel.send(content=message)
+    
+# 15 Minutes before the COTD
+@tasks.loop(time=datetime.time(hour=9, minute=45, second=0, tzinfo=datetime.timezone.utc))
+async def cotd_three_reminder(bot: Bot):
+    log.info('it is time for COTD Rerun #3')
+    
+    log.debug('Checking config file')
+    if constants.Bot.totd_reminders is False:
+        log.critical('TOTD Reminders are disabled')
+        return
+    
+    log.info('Pinging COTD #3 Role in TM India')
+    guild = bot.get_guild(constants.Guild.tmi_server)
+    
+    channel_id = constants.Channels.general if guild is not None else constants.Channel.testing_general
+    role_id = constants.Role.cotd_reminder_three if guild is not None else constants.Role.cotd_reminder_three_testing
+    
+    guild = guild if guild is not None else bot.get_guild(constants.Guild.testing_server)
+    
+    channel, role = bot.get_channel(channel_id), guild.get_role(role_id)
+    
+    message = f'{role.mention}: **COTD Reminder #3 starts in 15 minutes**\n\n:medal: Past COTD results: <https://trackmania.io/#/totd>'
+    
+    log.debug('Sending Message')
+    await channel.send(content=message)
+    
 async def _ping_api():
     api_client = APIClient()
 
