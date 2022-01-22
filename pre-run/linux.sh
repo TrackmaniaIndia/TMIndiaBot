2e4a7786b3d9876b9eda06e9aeb699ac36af7021
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2022-01-22 10:31:02 +0530

    linux pre-run script (#104)

---
+++ pre-run/linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ..
+echo "created logs"
+mkdir -p logs
+echo "created logs/commands.log"
+touch logs/commands.log
+echo "created bot/resources/temp"
+mkdir -p bot/resources/temp
+echo "created config.yaml"
+touch config.yaml
+echo "created bot/utils/node"
+mkdir -p bot/utils/node
