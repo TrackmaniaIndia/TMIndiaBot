df0f6977ad80f70742bf6e17594ab7b5fe70704f
Deepesh Nimma <deepnimma@gmail.com>
2022-04-11 10:18:26 +0530

    get_id command

--- bot/cogs/trackmania/get_id.py
+++ bot/cogs/trackmania/get_id.py
@@ -7,7 +7,6 @@ from bot import constants
 from bot.bot import Bot
 from bot.log import get_logger, log_command
 from bot.utils.discord import EZEmbed
-from bot.utils.trackmania import TrackmaniaUtils
 
 log = get_logger(__name__)
 
