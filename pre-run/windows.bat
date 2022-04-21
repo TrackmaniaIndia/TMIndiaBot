4ce190b3f56384d7a1573ad3bb31ab09eff638da
Ashwin Sharma <anupshrma2007@gmail.com>
2022-04-21 10:48:41 +0530

    fix syntax

--- pre-run/windows.bat
+++ pre-run/windows.bat
@@ -7,16 +7,16 @@ echo creating logs/commands.log
 touch logs/commands.log
 
 echo creating bot/resources/temp
-mkdir bot/resources/temp
+mkdir bot\resources\temp
 
 echo creating config.yaml
 touch config.yaml
 
 echo creating bot/utils/node
-mkdir bot/utils/node
+mkdir bot\utils\node
 
 echo creating bot/resources/temp
-mkdir -p bot/resources/temp
+mkdir bot\resources\temp
 
 echo creating times_run.txt
 echo 0 > bot/resources/times_run.txt
