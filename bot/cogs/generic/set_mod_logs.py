a09aa1fa5af0f4341c666d9fa483eb25289001af
Deepesh Nimma <deepnimma@gmail.com>
2022-04-24 17:38:07 +0530

    set mod logs command (works with pycord 2b5, fixed in github version

---
+++ bot/cogs/generic/set_mod_logs.py
@@ -0,0 +1,57 @@
+import json
+
+import discord
+from discord import ApplicationContext
+from discord.ext import commands
+
+from bot import constants
+from bot.bot import Bot
+from bot.log import get_logger, log_command
+
+log = get_logger(__name__)
+
+
+class SetModLog(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    # TODO: Set permissions. ManageServer
+    @commands.slash_command(
+        guild_ids=constants.Bot.default_guilds,
+        name="set-mod-logs",
+        description="Set a mod logs channel",
+    )
+    async def _set_mod_logs(
+        self, ctx: ApplicationContext, channel: discord.TextChannel
+    ):
+        log_command(ctx, "set_mod_logs")
+
+        await ctx.defer()
+
+        log.info(
+            "Saving mod-logs channel %s for guild %s", channel.name, ctx.guild.name
+        )
+        with open(
+            f"./bot/resources/guild_data/{ctx.guild.id}/config.json",
+            "r",
+            encoding="UTF-8",
+        ) as file:
+            config_data = json.load(file)
+
+        config_data["mod_logs_channel"] = channel.id
+
+        with open(
+            f"./bot/resources/guild_data/{ctx.guild.id}/config.json",
+            "w",
+            encoding="UTF-8",
+        ) as file:
+            json.dump(config_data, file, indent=4)
+
+        log.info("Stored ModLogs channel for %s", ctx.guild.name)
+        await ctx.respond(
+            f"Stored ModLogs channel for {ctx.guild.name} as {channel.name}"
+        )
+
+
+def setup(bot: Bot):
+    bot.add_cog(SetModLog(bot))
