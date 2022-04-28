77b5134f51a89e71ac28c3491168ecc4fbb9e549
Deepesh Nimma <deepnimma@gmail.com>
2022-04-28 11:05:43 +0530

    rename get-quote to show-quote

--- bot/cogs/quoting/show_quote.py
+++ bot/cogs/quoting/show_quote.py
@@ -9,26 +9,26 @@ from bot.log import get_logger, log_command
 log = get_logger(__name__)
 
 
-class GetQuote(commands.Cog):
+class ShowQuote(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.slash_command(
-        name="get-quote", description="Gets a quote of a specific number."
+        name="show-quote", description="Gets a quote of a specific number."
     )
-    async def _get_quote_cmd(
+    async def _show_quote(
         self,
         ctx: ApplicationContext,
         quote_number: Option(
             SlashCommandOptionType.integer, "The quote number to get", required=True
         ),
     ):
-        log_command(ctx, "get_quote")
+        log_command(ctx, "show_quote")
 
         await ctx.defer()
 
         log.debug("Getting the quote")
-        quote = quote_functions.get_quote_util(ctx.guild.id, quote_number)
+        quote = quote_functions.get_quote(ctx.guild.id, quote_number)
 
         if quote is None:
             await ctx.respond("That quote number does not exist.", ephemeral=True)
@@ -37,4 +37,4 @@ class GetQuote(commands.Cog):
 
 
 def setup(bot: Bot):
-    bot.add_cog(GetQuote(bot))
+    bot.add_cog(ShowQuote(bot))
