0cd8a002016ceec7fd0747f21572e1ae25ffc13b
Deepesh Nimma <56356841+NottCurious@users.noreply.github.com>
2022-04-28 21:04:15 +0530

    remove quote function and quotes number fixer (#233)

---
+++ bot/cogs/quoting/remove_quote.py
@@ -0,0 +1,69 @@
+import json
+
+import discord
+from discord import ApplicationContext, SlashCommandOptionType
+from discord.commands import Option
+from discord.ext import commands
+
+import bot.utils.commons as commons
+import bot.utils.quote as quote_functions
+from bot.bot import Bot
+from bot.log import get_logger, log_command
+from bot.utils.discord import create_embed
+
+log = get_logger(__name__)
+
+
+class RemoveQuote(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.slash_command(
+        name="remove-quote",
+        description="Removes the quote from the server's guild file",
+    )
+    @commands.has_permissions(manage_messages=True)
+    async def _remove_quote(
+        self,
+        ctx: ApplicationContext,
+        number: Option(
+            SlashCommandOptionType.integer,
+            "The number of the quote you want to remove",
+            required=True,
+        ),
+    ):
+        log_command(ctx, "remove-quote")
+
+        await ctx.defer()
+
+        log.info("Removing Quote #%s from %s", number, ctx.guild.name)
+        with open(
+            f"./bot/resources/guild_data/{ctx.guild.id}/quotes.json",
+            "r",
+            encoding="UTF-8",
+        ) as file:
+            aquotes = json.load(file)
+
+        log.debug("Popping the quote.")
+        try:
+            aquotes["quotes"].pop(number - 1)
+        except IndexError:
+            await ctx.respond("Invalid Number Given.", ephemeral=True)
+            return
+
+        log.debug("Fixing numbers.")
+        for i, _ in enumerate(aquotes["quotes"]):
+            aquotes["quotes"][i]["Number"] = i + 1
+
+        with open(
+            f"./bot/resources/guild_data/{ctx.guild.id}/quotes.json",
+            "w",
+            encoding="UTF-8",
+        ) as file:
+            json.dump(aquotes, file, indent=4)
+
+        await ctx.respond("Successfully Removed!", ephemeral=True)
+
+
+def setup(bot: Bot):
+    bot.add_cog(RemoveQuote(bot))
