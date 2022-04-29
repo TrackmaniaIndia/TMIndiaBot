ba78801bf80754896d27144bdfda1f63df8db766
Deepesh Nimma <deepnimma@gmail.com>
2022-04-29 09:33:24 +0530

    Create guild_data folder with pre-run files

--- pre-run/linux.sh
+++ pre-run/linux.sh
@@ -2,25 +2,28 @@
 cd ..
 
 echo "creating logs"
-mkdir -p logs
+mkdir -pv logs
 
 echo "creating logs/commands.log"
 touch logs/commands.log
 
 echo "creating bot/resources/temp"
-mkdir -p bot/resources/temp
+mkdir -pv bot/resources/temp
 
 echo "creating config.yaml"
 touch config.yaml
 
 echo "creating bot/utils/node"
-mkdir -p bot/utils/node
+mkdir -pv bot/utils/node
 
 echo "creating bot/resources/temp"
-mkdir -p bot/resources/temp
+mkdir -pv bot/resources/temp
 
 echo "creating times_run.txt"
 echo 0 > bot/resources/times_run.txt
+
+echo "creating guild_data folder"
+mkdir -pv bot/resources/guild_data
 
 echo "cloning TrackmaniaLeaderboard"
 git clone git@github.com:NottCurious/TrackmaniaLeaderboards.git bot/resources/leaderboard
