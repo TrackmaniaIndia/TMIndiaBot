a6af01b86f75bf6d84ef18064c8a7abf738f35d5
Deepesh Nimma <deepnimma@gmail.com>
2022-06-23 10:52:49 +0530

    Create the COTD Reminders and Royal Reminders setup commands

--- bot/cogs/config/reminders_setup.py
+++ bot/cogs/config/reminders_setup.py
@@ -52,6 +52,7 @@ class SetupReminders(commands.Cog):
         await ctx.defer()
 
         # Confirm where the reminders should be sent.
+        log.debug("Creating Confirmation for channel selection.")
         confirmer = Confirmer()
         confirmer.change_confirm_button(label="Yes!")
         confirmer.change_cancel_button(label="No!")
@@ -62,6 +63,7 @@ class SetupReminders(commands.Cog):
         )
         await confirmer.wait()
         await message.delete()
+        log.debug("Finished Confirmation for channel selection.")
 
         if confirmer.value is None:
             log.info("Confirmation Timed Out")
@@ -72,7 +74,7 @@ class SetupReminders(commands.Cog):
                 False, ctx.guild.id, 0, main_cotd, first_rerun, second_rerun, False
             )
             await ctx.send(
-                f"Settings Saved!\nYou have unsubscribed from Trophy Tracking.\nGuild Name: {ctx.guild.name} Channel Name: {ctx.channel.name}",
+                f"Settings Saved!\nYou have unsubscribed from COTD Reminders.\nGuild Name: {ctx.guild.name} Channel Name: {ctx.channel.name}",
                 delete_after=10,
             )
         elif confirmer.value:
@@ -87,11 +89,98 @@ class SetupReminders(commands.Cog):
                 False,
             )
             await ctx.send(
-                f"Settings Saved!\nYou have subscribed to Trophy Tracking.\nGuild Name: {ctx.guild.name} Channel Name: {ctx.channel.name}",
+                f"Settings Saved!\nYou have subscribed to COTD Reminders.\nGuild Name: {ctx.guild.name} Channel Name: {ctx.channel.name}",
                 delete_after=10,
+            )
+            log.info(
+                "Saved Settings. Main: %s, First Rerun: %s, Second Rerun: %s",
+                main_cotd,
+                first_rerun,
+                second_rerun,
+            )
+
+    @commands.slash_command(
+        name="setup-royal-reminders",
+        description="Start the setup process for Royal Reminders",
+    )
+    @commands.has_permissions(manage_guild=True)
+    async def _setup_royal_reminders(
+        self,
+        ctx: ApplicationContext,
+        main_royal: Option(
+            str,
+            "Whether to be reminded for the main Royal at xx:xxUTC",
+            choices=["Yes", "No"],
+            required=True,
+        ),
+        first_rerun: Option(
+            str,
+            "Whether to be reminded for the first Royal rerun at xx:xxUTC",
+            choices=["Yes", "No"],
+            required=True,
+        ),
+        second_rerun: Option(
+            str,
+            "Whether to be reminded for the second Royal rerun at xx:xxUTC",
+            choices=["Yes", "No"],
+            required=True,
+        ),
+    ):
+        log_command(ctx, "setup_cotd_reminders")
+
+        log.info("Starting setup process for %s", ctx.guild.name)
+        await ctx.defer()
+
+        # Confirm where the reminders should be sent.
+        log.debug("Creating Confirmation for channel selection.")
+        confirmer = Confirmer()
+        confirmer.change_confirm_button(label="Yes!")
+        confirmer.change_cancel_button(label="No!")
+
+        message = await ctx.respond(
+            "Is this the channel where you want your server's Royal reminders to be sent?\nIf this is not the channel you want, please re-run the command in that specific channel.\nIf you click `No` you will be unsubscribed from all reminders.",
+            view=confirmer,
+        )
+        await confirmer.wait()
+        await message.delete()
+        log.debug("Finished Confirmation for channel selection.")
+
+        if confirmer.value is None:
+            log.info("Confirmation Timed Out")
+            await ctx.send("Timed Out...", delete_after=10)
+        elif not confirmer.value:
+            log.info("Setup Declined")
+            self.__save_settings(
+                False, ctx.guild.id, 0, main_royal, first_rerun, second_rerun, True
+            )
+            await ctx.send(
+                f"Settings Saved!\nYou have unsubscribed from Royal Reminders.\nGuild Name: {ctx.guild.name} Channel Name: {ctx.channel.name}",
+                delete_after=10,
+            )
+        elif confirmer.value:
+            log.info("Setup Accepted")
+            self.__save_settings(
+                True,
+                ctx.guild.id,
+                ctx.channel.id,
+                main_royal,
+                first_rerun,
+                second_rerun,
+                True,
+            )
+            await ctx.send(
+                f"Settings Saved!\nYou have subscribed to Royal Reminders.\nGuild Name: {ctx.guild.name} Channel Name: {ctx.channel.name}",
+                delete_after=10,
+            )
+            log.info(
+                "Saved Settings. Main: %s, First Rerun: %s, Second Rerun: %s",
+                main_royal,
+                first_rerun,
+                second_rerun,
             )
 
     def __save_settings(
+        self,
         subscribe: bool,
         guild_id: int,
         channel_id: int,
@@ -126,14 +215,14 @@ class SetupReminders(commands.Cog):
         ) as file:
             config_data = json.load(file)
 
-        config_data["reminder_channel"] = channel_id
-
         # Checking if it was called from `/setup-cotd-tracking` or `/setup-royal-tracking`.
         if not royal:
+            config_data["cotd_reminder_channel"] = channel_id
             config_data["cotd_one_reminder"] = main
             config_data["cotd_two_reminder"] = first_rerun
             config_data["cotd_three_reminder"] = second_rerun
         else:
+            config_data["royal_reminder_channel"] = channel_id
             config_data["royal_one_reminder"] = main
             config_data["royal_two_reminder"] = first_rerun
             config_data["royal_three_reminder"] = second_rerun
