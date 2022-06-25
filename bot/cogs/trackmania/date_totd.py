94b6168e17bdadd947d1fdea03940e3f831d0731
Deepesh Nimma <deepnimma@gmail.com>
2022-06-25 12:20:22 +0530

    create parse_totd_data command

--- bot/cogs/trackmania/date_totd.py
+++ bot/cogs/trackmania/date_totd.py
@@ -10,53 +10,9 @@ from bot import constants
 from bot.bot import Bot
 from bot.log import get_logger, log_command
 from bot.utils.discord import ViewAdder, create_embed
+from bot.utils.totd import MAP_TYPE_ENUMS, parse_totd_data
 
 log = get_logger(__name__)
-
-MAP_TYPE_ENUMS: dict = {
-    1: "Race",
-    2: "FullSpeed",
-    3: "Tech",
-    4: "RPG",
-    5: "LOL",
-    6: "Press Forward",
-    7: "SpeedTech",
-    8: "MultiLap",
-    9: "Offroad",
-    10: "Trial",
-    11: "ZrT",
-    12: "SpeedFun",
-    13: "Competitive",
-    14: "Ice",
-    15: "Dirt",
-    16: "Stunt",
-    17: "Reactor",
-    18: "Platform",
-    19: "Slow Motion",
-    20: "Bumper",
-    21: "Fragile",
-    22: "Scenery",
-    23: "Kacky",
-    24: "Endurance",
-    25: "Mini",
-    26: "Remake",
-    27: "Mixed",
-    28: "Nascar",
-    29: "SpeedDrift",
-    30: "Minigame",
-    31: "Obstacle",
-    32: "Transitional",
-    33: "Grass",
-    34: "Backwards",
-    35: "Freewheel",
-    36: "Signature",
-    37: "Royal",
-    38: "Water",
-    39: "Plastic",
-    40: "Arena",
-    41: "N/A",
-    42: "Educational",
-}
 
 
 class LatestTOTD(commands.Cog):
@@ -75,7 +31,7 @@ class LatestTOTD(commands.Cog):
         log.debug("Getting TOTD Data")
         totd_data = await TOTD.latest_totd()
         log.debug("Parsing Data")
-        page, buttons = await self.__parse_pages(totd_data)
+        page, buttons = await parse_totd_data(totd_data)
 
         log.debug("Sending Embed")
         await ctx.respond(embed=page, view=ViewAdder(buttons))
@@ -130,7 +86,7 @@ class LatestTOTD(commands.Cog):
             return
 
         log.debug("Parsing Data")
-        page, buttons = await self.__parse_pages(totd_data)
+        page, buttons = await parse_totd_data(totd_data)
 
         log.debug("Sending Embed")
         await ctx.respond(embed=page, view=ViewAdder(buttons))
@@ -197,6 +153,22 @@ class LatestTOTD(commands.Cog):
         )
 
         return page_one, [map_download_button, tmio_url_button, tmx_button]
+
+
+async def _parse_totd_data(
+    totd_data: TOTD,
+) -> tuple[discord.Embed, list[discord.ui.Button]]:
+    log.debug("Parsing TOTD Values")
+    map_name = totd_data.map.name
+    author_name = totd_data.map.author_name
+    _todays_date = datetime.now()
+
+    title_string = f"Track of the Day of {commons.get_ordinal_number(totd_data.month_day)} {constants.Consts.months[_todays_date.month - 1]}"
+    nadeo_uploaded = f"<t:{int(totd_data.map.uploaded.timestamp())}:R>"
+    map_download = totd_data.map.url
+    tmio_url = f"https://trackmania.io/#/leaderboard/{totd_data.map.uid}"
+
+    log.debug("Creating Embed")
 
 
 def setup(bot: Bot):
